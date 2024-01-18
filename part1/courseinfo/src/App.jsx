7b27cb4680ce6deee331d4f89283382bf5a1cd2d
sohero <46505424+sohero31@users.noreply.github.com>
2024-01-18 13:27:42 +1100

    1.5 step5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -53,9 +53,9 @@ const Total = (props) => {
 
 const App = () => {
   
-  
-    const course = 'Half Stack application development'
-    const parts = [
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
       {
         name: 'Fundamentals of React',
         exercises: 10
@@ -69,21 +69,22 @@ const App = () => {
         exercises: 14
       }
     ]
+  }
     
   return (
     <div>
-      <Header course = {course} />
+      <Header course = {course.name} />
       
       <Content 
-      part1 = {parts[0].name} 
-      exercises1 = {parts[0].exercises} 
-      part2 = {parts[1].name} 
-      exercises2 = {parts[1].exercises} 
-      part3 = {parts[2].name}
-      exercises3 = {parts[2].exercises}
+      part1 = {course.parts[0].name} 
+      exercises1 = {course.parts[0].exercises} 
+      part2 = {course.parts[1].name} 
+      exercises2 = {course.parts[1].exercises} 
+      part3 = {course.parts[2].name}
+      exercises3 = {course.parts[2].exercises}
       />
 
-      <Total total = {parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+      <Total total = {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
       </div>
   )
 }
